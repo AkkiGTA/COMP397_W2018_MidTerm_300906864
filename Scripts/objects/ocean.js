ae63bb1bd789fad51e0bda8aa1849322d7393fb3
AkkiGTA <akkigta@gmail.com>
2018-03-24 17:59:59 -0400

    Update ocean.js

--- Scripts/objects/ocean.js
+++ Scripts/objects/ocean.js
@@ -14,29 +14,51 @@ var objects;
         __extends(Ocean, _super);
         // public properties
         // Constructor
-        function Ocean() {
+        function Ocean(lvl) {
             var _this = _super.call(this, managers.Game.assetManager.getResult("ocean")) || this;
+            _this._lvl = lvl;
+            if (_this._lvl == 2) {
+                _this.rotation = 90;
+            }
             _this.Start();
             return _this;
         }
         // private methods
         // reset the objects location to some value
         Ocean.prototype._reset = function () {
-            this.y = -960;
+            if (this._lvl == 1) {
+                this.y = -960;
+            }
+            else if (this._lvl == 2) {
+                this.x = 1420;
+            }
         };
         // move the object to some new location
         Ocean.prototype._move = function () {
-            this.y += this._dy;
+            if (this._lvl == 1) {
+                this.y += this._dy;
+            }
+            else if (this._lvl == 2) {
+                this.x -= this._dx;
+            }
         };
         // check to see if some boundary has been passed
         Ocean.prototype._checkBounds = function () {
-            if (this.y >= 0) {
-                this._reset();
+            if (this._lvl == 1) {
+                if (this.y >= 0) {
+                    this._reset();
+                }
+            }
+            else if (this._lvl == 2) {
+                if (this.x <= 640) {
+                    this._reset();
+                }
             }
         };
         // public methods
         // Initializes variables and creates new objects
         Ocean.prototype.Start = function () {
+            this._dx = 5;
             this._dy = 5;
             this._reset();
         };
