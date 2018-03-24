a4fcd49b3ad1054dbe0e07fdaf7b41f8386c8bc0
AkkiGTA <akkigta@gmail.com>
2018-03-24 17:57:31 -0400

    Update planeflash.ts

--- Scripts/objects/planeflash.ts
+++ Scripts/objects/planeflash.ts
@@ -1,12 +1,16 @@
 module objects {
   export class PlaneFlash extends objects.GameObject {
     // private instance variables
-
+    private _lvl:number;
     // public properties
 
     // constructors
-    constructor() {
+    constructor(lvl:number) {
       super("planeflash");
+      this._lvl = lvl;
+      if(this._lvl == 2) {
+        this.rotation = 90;
+      }
     }
 
     // private methods
